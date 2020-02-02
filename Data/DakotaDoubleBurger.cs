84c3f83c0408079bd6c96952effc0b219f0ec2ad
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-02 14:24:07 -0600

    Added class for the dakota double burger

---
+++ Data/DakotaDoubleBurger.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// a class representing the dakota double burger entree
+    /// </summary>
+    public class DakotaDoubleBurger
+    {
+        public double Price
+        {
+            get
+            {
+                return 5.20;
+            }
+        }
+
+        public uint Calories
+        {
+            get
+            {
+                return 464;
+            }
+        }
+
+        /// <summary>
+        /// if the double burger has a bun
+        /// </summary>
+        private bool bun = true;
+        public bool Bun
+        {
+            get { return bun; }
+            set { bun = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has ketchup
+        /// </summary>
+        private bool ketchup = true;
+        public bool Ketchup
+        {
+            get { return ketchup; }
+            set { ketchup = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has mustard
+        /// </summary>
+        private bool mustard = true;
+        public bool Mustard
+        {
+            get { return mustard; }
+            set { mustard = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has a pickle
+        /// </summary>
+        private bool pickle = true;
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has cheese
+        /// </summary>
+        private bool cheese = true;
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { cheese = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has tomato
+        /// </summary>
+        private bool tomato = true;
+        public bool Tomato
+        {
+            get { return tomato; }
+            set { tomato = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has lettuce
+        /// </summary>
+        private bool lettuce = true;
+        public bool Lettuce
+        {
+            get { return lettuce; }
+            set { lettuce = value; }
+        }
+
+        /// <summary>
+        /// if the double burger has mayo
+        /// </summary>
+        private bool mayo = true;
+        public bool Mayo
+        {
+            get { return mayo; }
+            set { mayo = value; }
+        }
+
+        /// <summary>
+        /// special instructins for the preparation of the double burger
+        /// </summary>
+        public List<String> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bun) instructions.Add("hold bun");
+                if (!ketchup) instructions.Add("hold ketchup");
+                if (!mustard) instructions.Add("hold mustard");
+                if (!pickle) instructions.Add("hold pickle");
+                if (!cheese) instructions.Add("hold cheese");
+                if (!tomato) instructions.Add("hold tomato");
+                if (!lettuce) instructions.Add("hold lettuce");
+                if (!mayo) instructions.Add("hold mayo");
+
+                return instructions;
+
+            }
+        }
+    }
+}
