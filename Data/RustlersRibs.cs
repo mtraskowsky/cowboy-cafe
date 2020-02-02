ef067b759f1ef0a294b76fd32b618a789302c18b
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-01 19:47:54 -0600

    Created class for Rustler's ribs

---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Rustler's Ribs entree
+    /// </summary>
+    public class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the Rustler's Ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+    }
+}
