faf5fb5ca56a3f8f60862510985b38b2149f4620
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-05 15:09:23 -0600

    added entree base class

---
+++ Data/Entree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    ///  a base class representing an entree
+    /// </summary>
+    public abstract class Entree
+    {
+        /// <summary>
+        /// get the price of the entree
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// get the calories of the entree
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// get the special instructions to go with the entree
+        /// </summary>
+        public abstract List<string> SpecialInstructions { get;  }
+
+    }
+}
