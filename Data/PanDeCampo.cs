40920ff806c07953522533fb54480507d2b56d3d
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-06 08:10:24 -0600

    Added class for the pan de campo

---
+++ Data/PanDeCampo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the pan de campo side
+    /// </summary>
+    public class PanDeCampo : Side
+    {
+        /// <summary>
+        /// the calories of the pan de campo side, depending on size
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 227;
+                    case Size.Medium:
+                        return 269;
+                    case Size.Large:
+                        return 367;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// the price of the pan de campo, depending on size
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.59;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Large:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
