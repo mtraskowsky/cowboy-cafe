1e4a350fd47232f14bc7920f38c52f64a5c77d04
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-07 09:40:53 -0600

    tested baked beans

--- Data/BakedBeans.cs
+++ Data/BakedBeans.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class BakedBeans
+    /// <summary>
+    /// A class representing the baked beans side
+    /// </summary>
+    public class BakedBeans :Side
     {
+        /// <summary>
+        /// the calories of the baked beans side, depending on size
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 312;
+                    case Size.Medium:
+                        return 378;
+                    case Size.Large:
+                        return 410;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// the price of the baked beans side, depending on size
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.59;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Large:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
     }
 }
