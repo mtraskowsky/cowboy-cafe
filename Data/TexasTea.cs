102349119db5eee6fe985307f5573be8877748f7
Maria Traskowsky <46056263+mtraskowsky@users.noreply.github.com>
2020-02-12 17:16:39 -0600

    Update TexasTea.cs

--- Data/TexasTea.cs
+++ Data/TexasTea.cs
@@ -1,10 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class TexasTea
+    public class TexasTea : Drink
     {
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.00;
+                    case Size.Medium:
+                        return 1.50;
+                    case Size.Large:
+                        return 2.00;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
     }
 }
