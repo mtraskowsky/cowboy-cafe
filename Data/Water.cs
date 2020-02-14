a0df4cbbc749cd1ef2d7bb4891768d59abb7483e
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-13 21:13:30 -0600

    tset water

--- Data/Water.cs
+++ Data/Water.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class Water
+    public class Water : Drink
     {
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 0.12;
+                    case Size.Medium:
+                        return 0.12;
+                    case Size.Large:
+                        return 0.12;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
     }
 }
