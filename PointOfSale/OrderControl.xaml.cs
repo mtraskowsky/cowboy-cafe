8b2a4e28d62a19c5ad8a3389332b8ea4199ce57e
Maria Traskowsky <mariatraskowsky@users.campus>
2020-02-19 14:28:28 -0600

    inclass

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CowboyCafe.Data;
 
 namespace PointOfSale
 {
@@ -21,8 +22,12 @@ namespace PointOfSale
         public OrderControl()
         {
             InitializeComponent();
-            //AddTrailBurgerButton.Height = 90;
-            // event handler //AddTrailBurgerButton.Click 
+            AddCowpokeChiliButton.Click += OnAddCowpokeChiliButtonClicked;
+        }
+
+        void OnAddCowpokeChiliButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CowpokeChili());
         }
     }
 }
